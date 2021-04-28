d2596ef8e4b5d1c8bad0acac0c8c24c56acaac4e
JuanchoVicente <83217922+JuanchoVicente@users.noreply.github.com>
2021-04-28 11:54:04 +0200

    Add files via upload

---
+++ ServiceDepartamentosRDSMSql/Controllers/DepartamentosController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceDepartamentosRDSMSql.Models;
+using ServiceDepartamentosRDSMSql.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceDepartamentosRDSMSql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentosController : ControllerBase
+    {
+        RepositoryDepartamentos repo;
+
+        public DepartamentosController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+
+        /// <summary>
+        /// Lista de departamentos
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<Departamento> GetDepartamentos()
+        {
+            return this.repo.GetDepartamentos();
+        }
+
+        /// <summary>
+        /// Buscar un departamento por su identificador
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>       
+        [HttpGet("{id}")]
+        public Departamento FindDepartamento(int id)
+        {
+            return this.repo.FindDepartamento(id);
+        }
+
+    }
+}
---
+++ ServiceDepartamentosRDSMSql/Data/DepartamentoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceDepartamentosRDSMSql.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceDepartamentosRDSMSql.Data
+{
+    public class DepartamentoContext: DbContext
+    {
+        public DepartamentoContext(DbContextOptions<DepartamentoContext> options) 
+            :base(options) { } 
+
+        public DbSet<Departamento> Departamentos { get; set; }
+    }
+}
